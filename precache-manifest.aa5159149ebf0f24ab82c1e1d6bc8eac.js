5fb79253a2984c8a9d4ad5c6f3f6c3123b4c97fb
SteveImmanuel <steveandreasimanuel@gmail.com>
2019-06-20 16:09:09 +0700

    Updates

---
+++ precache-manifest.aa5159149ebf0f24ab82c1e1d6bc8eac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d3f3e45d74582393f059eecd6b771b0",
+    "url": "/OeklaNIMFinder/index.html"
+  },
+  {
+    "revision": "bc990fa86e52122c46bb",
+    "url": "/OeklaNIMFinder/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0f67545457bbb7496837",
+    "url": "/OeklaNIMFinder/static/css/main.c335a78b.chunk.css"
+  },
+  {
+    "revision": "bc990fa86e52122c46bb",
+    "url": "/OeklaNIMFinder/static/js/2.125a33c0.chunk.js"
+  },
+  {
+    "revision": "0f67545457bbb7496837",
+    "url": "/OeklaNIMFinder/static/js/main.a299eeba.chunk.js"
+  },
+  {
+    "revision": "1c17c092ca30638b1fd2",
+    "url": "/OeklaNIMFinder/static/js/runtime~main.38a6d8af.js"
+  }
+]);
